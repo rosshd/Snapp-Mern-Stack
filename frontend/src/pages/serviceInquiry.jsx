a97b1c459a947aa3411874bf2c82d7c66b7dd063
rosshd <rosshdawson@gmail.com>
2024-07-04 23:21:03 -0400

    updated form look

--- frontend/src/pages/serviceInquiry.jsx
+++ frontend/src/pages/serviceInquiry.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import NavBar from '../components/navBar.jsx'
 import Form from "../components/form.jsx"
 import snapLogoBanner from "../assets/mainLogo.png"
+import logoIcon from "../assets/logoIcon.png"
 
 const shop = () => {
   return (
-    <div className='flex flex-col justify-center items-center h-screen bg-zinc-800'>
-      <NavBar className='rounded-xl bg-zinc-800'></NavBar>
-      <img src={snapLogoBanner} className='mt-[5%] h-[10%]'></img>
+    <div className='flex flex-col justify-center items-center h-screen bg-zinc-900'>
+      <NavBar className='rounded-xl bg-zinc-900'></NavBar>
+      <img src={snapLogoBanner} className='mb-4 mt-[2%] h-[10%]'></img>
       <Form />
 
     </div>
