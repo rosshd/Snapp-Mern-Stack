78b50aee766a8e7f50e1ee4ba2577fbbf6f8022a
rosshd <rosshdawson@gmail.com>
2024-07-12 00:54:55 -0400

    create services// gallery database

--- frontend/src/pages/CreateService.jsx
+++ frontend/src/pages/CreateService.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
+const Title = '';
+const Description = '';
+
+const titleHandler = function(e) {
+  result.Title = e.target.value;
+}
+const descriptionHandler = function(e) {
+  result.Title = e.target.value;
+}
+const hrefHandler = function(e) {
+  result.Title = e.target.value;
+}
+
 const CreateService = () => {
   return (
     <div>
-      <h1>service uploader</h1>
+      <form method='post' encType='multipart/form-data' action="/gallery">
+        <input type="text" placeholder='Title' onChange={titleHandler}/>
+        <input type="text" placeholder='Title' onChange={descriptionHandler}/>
+        <input type="text" placeholder='Link' onChange={hrefHandler} />
+        <input type="file" name="file"/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   )
-}
+};
 
 export default CreateService
--- frontend/src/pages/ServicePage.jsx
+++ frontend/src/pages/ServicePage.jsx
@@ -22,6 +22,48 @@ const ServicePage = () => {
     });
   }, []);
 
+  const leftDiv = ( services ) => (
+    <div className='h-[75vh] flex flex-col w-screen justify-center max-md:items-center bg-zinc-50'>
+      <div className='h-[50%] flex flex-row justify-center gap-[5%] items-center w-[60%] py-10 bg-zinc-700 dark:bg-zinc-900 rounded-3xl md:ml-[3%] border-orange-300 border-y-8'>
+        <div className='flex flex-row gap-2'>
+          <div className='h-full flex flex-col justify-center items-stretch overflow-hidden'>
+            <img src={ironsuit1} className='h-[31vh]'/>
+            </div>
+          <div className='h-full flex flex-col justify-center gap-[1vh] items-center overflow-hidden'>
+            <img src={ironsuit1} className='h-[15vh]'/>
+            <img src={ironsuit1} className='h-[15vh]'/>
+          </div>
+        </div>
+        <div className='h-full w-[50%] rounded-lg bg-opacity-20 bg-zinc-900 px-8 flex flex-col items-center justify-center'>
+          <h1 className='text-white text-center text-xs'>{services.Description}
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+
+  const rightDiv = (  services ) => (
+    <div className='h-[75vh] flex flex-col w-screen justify-center max-md:items-center bg-zinc-50'>
+      <div className='h-[50%] flex flex-row justify-center self-end gap-[5%] items-center w-[60%] py-10 bg-zinc-700 dark:bg-zinc-900 rounded-3xl md:mr-[3%] border-orange-300 border-y-8'>
+        <div className='flex flex-row gap-2'>
+          <div className='h-full flex flex-col justify-center items-stretch overflow-hidden'>
+            <img src={ironsuit1} className='h-[31vh]'/>
+            </div>
+          <div className='h-full flex flex-col justify-center gap-[1vh] items-center overflow-hidden'>
+            <img src={ironsuit1} className='h-[15vh]'/>
+            <img src={ironsuit1} className='h-[15vh]'/>
+          </div>
+        </div>
+        <div className='h-full w-[50%] rounded-lg bg-opacity-20 bg-zinc-900 px-8 flex flex-col items-center justify-center'>
+          <h1 className='text-white text-center text-xs'>{services.Description}
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+
+
+
   return (
     <div className='flex flex-col w-screen dark:bg-zinc-700 bg-zinc-50 pt-8 items-center'>
       <h1 className='text-center font-rancho text-9xl text-black rounded-2xl border-b-orange-300 border-b-8'>Gallery</h1>
@@ -30,25 +72,10 @@ const ServicePage = () => {
           <Spinner className='fixed align-middle justify-self-center'/>
         ) : (
           services.map((services, index) => (
-          <div className='h-[75vh] flex flex-col w-screen justify-center max-md:items-center bg-zinc-50'>
-            <div className='h-[50%] flex flex-row justify-center gap-[5%] items-center w-[60%] py-10 bg-zinc-700 dark:bg-zinc-900 rounded-3xl md:ml-[3%] border-orange-300 border-y-8'>
-              <div className='flex flex-row gap-2'>
-                <div className='h-full flex flex-col justify-center items-stretch overflow-hidden'>
-                  <img src={ironsuit1} className='h-[31vh]'/>
-                  </div>
-                <div className='h-full flex flex-col justify-center gap-[1vh] items-center overflow-hidden'>
-                  <img src={ironsuit1} className='h-[15vh]'/>
-                  <img src={ironsuit1} className='h-[15vh]'/>
-                </div>
-              </div>
-              <div className='h-full w-[50%] rounded-lg bg-opacity-20 bg-zinc-900 px-8 flex flex-col items-center justify-center'>
-                <h1 className='text-white text-center'>{services.Description}
-                </h1>
-              </div>
-            </div>
-          </div>
-          )
-        ))};
+            index % 2 == 0 
+            ? leftDiv(services)
+            : rightDiv(services)
+        )))}
     </div>
   )
 }
