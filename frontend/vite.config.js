0d7f2d74d3b4b297c8567526270dba3f1a2c54e7
rosshd <rosshdawson@gmail.com>
2024-09-23 22:25:37 -0400

    run commands updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  base: process.env.NODE_ENV === 'production' ? '/your-subpath/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
 });
