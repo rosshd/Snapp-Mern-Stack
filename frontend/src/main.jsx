23c02ce9a43b46a6ac362b200b33c8cab6e3eefb
rosshd <rosshdawson@gmail.com>
2024-10-03 11:11:36 -0400

    revers speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
