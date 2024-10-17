888425831ed86bf87e5b3b1881a8c7849437b6a3
rosshd <rosshdawson@gmail.com>
2024-10-17 12:24:49 -0400

    favicon fix

--- frontend/src/pages/serviceInquiry.jsx
+++ frontend/src/pages/serviceInquiry.jsx
@@ -6,7 +6,7 @@ import NavBar from '../components/navBar.jsx';
 
 const Shop = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-zinc-200 dark:bg-zinc-900">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-zinc-300 dark:bg-zinc-900">
       <NavBar className="bg-zinc-900" />
       <div className="flex max-lg:flex-col pt-12 flex-row justify-center items-center md:h-[10%] w-[300px] max-md:w-[200px] space-x-2 mb-4 max-sm:mb-2 relative">
         {/* Adjusting image sizes for different screen sizes */}
