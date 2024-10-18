a0d3b154ec0e1c73974243029258d9bd39f28ad0
rosshd <rosshdawson@gmail.com>
2024-10-18 11:58:06 -0400

    redo lazyloading homepage

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,12 +2,12 @@ import React, { Suspense, lazy } from 'react';
 import Heading from '../components/heading';
 import NavBar from '../components/navBar';
 import Projects from '../components/projectLanding';
-import Uses from '../components/uses'
-import ContSoci from '../components/contSocials';
+const Uses = lazy(() => import('../components/uses'));
+const ContSoci = lazy(() => import('../components/contSocials'));
 
 const Home = () => {
   return (
-    <div className='overflow-y-auto bg-zinc-300 dark:bg-zinc-900 justify-center items-center'>
+    <div className=' overflow-y-auto bg-zinc-300 dark:bg-zinc-900 justify-center items-center'>
   
       <Heading />
       
@@ -16,17 +16,22 @@ const Home = () => {
         <div className='w-screen h-1 bg-zinc-100 dark:bg-zinc-900 flex flex-col justify-center items-center'>
           <div className='w-72 max-md:w-36 h-1 rounded-lg bottom-[100%] self-center justify-self-center bg-orange-300 hidden max-md:block dark:block'/>
         </div>
+      
       <Projects />
       
         <div className='w-screen flex h-1 bg-zinc-300 dark:bg-zinc-900 flex-col justify-center items-center'>
           <div className='w-72 max-md:w-36 h-1 rounded-lg bottom-[100%] self-center justify-self-center bg-amber-400 hidden max-md:block dark:block'/>
         </div>
-      <Uses />
+      <Suspense>
+        <Uses />
+      </Suspense>
       
         <div className='w-screen flex h-1 bg-zinc-300 dark:bg-zinc-900 flex-col justify-center items-center'>
           <div className='w-72 max-md:w-36 h-1 rounded-lg bottom-[100%] self-center justify-self-center bg-amber-400 hidden max-md:block dark:block'/>
         </div>
-      <ContSoci />
+      <Suspense> 
+        <ContSoci />
+      </Suspense> 
     </div>
   );
 }
